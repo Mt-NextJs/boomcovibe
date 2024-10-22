3b80749c66939f93a3dd25dc3e146933d1f5efbf
printilikepenguin <hanguel97@gmail.com>
2024-10-23 07:20:49 +0900

    Feat: 메인페이지 구현

--- src/app/(intro)/page.tsx
+++ src/app/(intro)/page.tsx
@@ -1,21 +1,42 @@
 import Image from 'next/image';
-import Link from "next/link";
-import {ClientRoute} from "@config/route";
+import Link from 'next/link';
+import BlockStack from '@components/main-stack';
+import { ClientRoute } from '@config/route';
 
 export default function Intro() {
     return (
-        <div className={'w-full flex flex-col items-center py-10'}>
-            <Image
-                className="dark:invert"
-                src="https://nextjs.org/icons/next.svg"
-                alt="Next.js logo"
-                width={180}
-                height={38}
-                priority
-            />
-            <Link href={ClientRoute.LOGIN as string} className={'button color'}>
-                시작하기
-            </Link>
+        <div className="flex h-dvh flex-col justify-between bg-primary-100 py-[20%]">
+            <div className="flex h-full items-center">
+                <div className="flex w-full items-center gap-[2rem]">
+                    <BlockStack />
+                    <div className="flex w-[12rem] flex-col justify-center gap-[1rem]">
+                        <div className="logo-desc text-foreground w-full text-xl">
+                            <div className="transform text-left transition duration-500 hover:translate-x-2 hover:text-red-300">
+                                블록으로
+                            </div>
+                            <div className="transform text-center transition duration-500 hover:scale-105 hover:text-red-300">
+                                표현하는
+                            </div>
+                        </div>
+                        <div className="logo-inmylink w-full text-center">
+                            <div className="text-foreground p-2 text-2xl font-extrabold tracking-wider duration-500 hover:scale-105 hover:text-red-300">
+                                인마이링크
+                            </div>
+                            <div className="text-lg text-primary-250">
+                                In My Link
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="loginButtons absolute bottom-[10%] flex min-w-[25rem] flex-col items-center gap-[2rem] self-center">
+                <Link
+                    href={ClientRoute.LOGIN as string}
+                    className={'button color'}
+                >
+                    시작하기
+                </Link>
+            </div>
         </div>
     );
 }
---
+++ src/components/main-stack.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+
+export default function BlockStack() {
+    return (
+        <div className="relative flex h-[40rem] min-w-[23rem] flex-col">
+            <Image
+                src="/assets/icons/icon-block1.png"
+                alt="block1"
+                width={230}
+                height={155}
+                className="loading-block-first absolute z-30 transform transition duration-500 hover:-translate-y-20"
+            />
+            <Image
+                src="/assets/icons/icon-block2.png"
+                alt="block2"
+                width={230}
+                height={155}
+                className="loading-block-second absolute z-20 transform transition duration-500 hover:-translate-y-20"
+            />
+            <Image
+                src="/assets/icons/icon-block1.png"
+                alt="block3"
+                width={230}
+                height={192}
+                className="loading-block-third absolute z-10 transform transition duration-500 hover:-translate-y-20"
+            />
+            <Image
+                src="/assets/icons/icon-block2.png"
+                alt="block4"
+                width={230}
+                height={236}
+                className="loading-block-fourth absolute z-0 transform transition duration-500 hover:translate-x-20"
+            />
+        </div>
+    );
+}
