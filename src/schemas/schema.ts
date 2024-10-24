41bfe849148b229962e2ef0aa83e8b56a3eddf71
thefigtree <ldj8431@naver.com>
2024-10-24 14:32:36 +0900

    Fix 수정 Merge Issue

--- src/schemas/schema.ts
+++ src/schemas/schema.ts
@@ -1,24 +1,24 @@
-import { z } from 'zod';
+// import { z } from 'zod';
 
-export const joinFormSchema = z.object({
-    // login, join 폴더를 위한 유효성 검사
-    name: z.string().min(4, 'Username is required').max(100),
-    userId: z.string().min(4),
-    password: z
-        .string()
-        .refine(
-            (val) =>
-                /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(
-                    val,
-                ),
-            {
-                message:
-                    '비밀번호는 최소 8문자 이상이어야 하며 최소한 1개의 숫자, 대문자와 특수문자를 포함해야 합니다.',
-            },
-        ),
-    // confirmPassword: z.string(),
-    email: z.string().trim().email(),
-});
+// export const joinFormSchema = z.object({
+//     // login, join 폴더를 위한 유효성 검사
+//     name: z.string().min(4, 'Username is required').max(100),
+//     userId: z.string().min(4),
+//     password: z
+//         .string()
+//         .refine(
+//             (val) =>
+//                 /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(
+//                     val,
+//                 ),
+//             {
+//                 message:
+//                     '비밀번호는 최소 8문자 이상이어야 하며 최소한 1개의 숫자, 대문자와 특수문자를 포함해야 합니다.',
+//             },
+//         ),
+//     // confirmPassword: z.string(),
+//     email: z.string().trim().email(),
+// });
 // .superRefine((val, ctx) => {
 //     if (val.password !== val.confirmPassword) {
 //         ctx.addIssue({
