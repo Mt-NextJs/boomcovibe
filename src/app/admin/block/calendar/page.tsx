7ed1a192d6c525d80bd5aa52f8f1b98695b71706
printilikepenguin <hanguel97@gmail.com>
2024-10-24 09:21:29 +0900

    Fix: 빌드 오류 수정

--- src/app/admin/block/calendar/page.tsx
+++ src/app/admin/block/calendar/page.tsx
@@ -1,159 +1,180 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import "./component/schedule.css";
-import ScheduleList from "./component/schedule-list";
-import BlockHeader from "../components/block-header";
-import PreblockCalendarOne from "../components/preview/preblock-cal-one";
-import PreblockCalendarTwo from "../components/preview/preblock-cal-two";
-import useBlockStore from "store/useBlockStore";
+import Link from 'next/link';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import './component/schedule.css';
+import ScheduleList from './component/schedule-list';
+import BlockHeader from '../components/block-header';
+import PreblockCalendarOne from '../components/preview/preblock-cal-one';
+import PreblockCalendarTwo from '../components/preview/preblock-cal-two';
+import useBlockStore from 'store/useBlockStore';
 
 export default function CalendarBlock() {
-  const [dataSet, setDataSet] = useState<boolean>(false);
-  const { blocks } = useBlockStore();
-  const [isListView, setIsListView] = useState(true);
-  const [viewTypeNow, setViewTypeNow] = useState(true);
-  useEffect(() => {
-    blocks ? setDataSet(true) : setDataSet(false);
-  }, [blocks]);
-  return (
-    <>
-      <BlockHeader
-        windowIcon={"/assets/icons/icon_close.png"}
-        iconLink={"/admin"}
-        blockTitle={"캘린더 블록"}
-        blockDescription={`진행/예정된 일정이 1개 이상이어야
+    const [dataSet, setDataSet] = useState<boolean>(false);
+    const { blocks } = useBlockStore();
+    const [isListView, setIsListView] = useState(true);
+    const [viewTypeNow, setViewTypeNow] = useState(true);
+
+    useEffect(() => {
+        if (blocks) {
+            setDataSet(true);
+        } else {
+            setDataSet(false);
+        }
+    }, [blocks]);
+    return (
+        <>
+            <BlockHeader
+                windowIcon={'/assets/icons/icon_close.png'}
+                iconLink={'/admin'}
+                blockTitle={'캘린더 블록'}
+                blockDescription={`진행/예정된 일정이 1개 이상이어야
                   <br>
                   캘린더 블록을 공개할 수 있습니다`}
-      />
-      <div className="px-10">
-        <Link href={"/admin/block/calendar/form"}>
-          <button className="button color">+ 캘린더에 일정을 추가하세요</button>
-        </Link>
-      </div>
-
-      <div className="mb-10 h-3 w-full bg-gray-100" />
-
-      <div className="px-10">
-        <div>
-          <p className="mb-4 text-2xl font-semibold text-gray-700">
-            스타일설정
-          </p>
-          <div className="mb-4 flex flex-wrap">
-            <div className="flex gap-4">
-              <div
-                className="flex cursor-pointer"
-                onClick={() => setIsListView(true)}>
-                <div
-                  className={`circle mr-2 flex h-6 w-6 items-center justify-center rounded-full border-2 ${
-                    isListView
-                      ? "bg-background border-4 border-orange-500"
-                      : "border-gray-200"
-                  }`}>
-                  <div
-                    className={`button-color h-3 w-3 rounded-full ${
-                      isListView ? "border-background border-2 bg-primary" : ""
-                    }`}></div>
-                </div>
-                <div>리스트뷰</div>
-              </div>
-              <div
-                className="flex cursor-pointer"
-                onClick={() => setIsListView(false)}>
-                <div
-                  className={`circle mr-2 flex h-6 w-6 items-center justify-center rounded-full border-2 ${
-                    !isListView
-                      ? "bg-background border-4 border-primary"
-                      : "border-gray-200"
-                  }`}>
-                  <div
-                    className={`button-color h-3 w-3 rounded-full ${
-                      !isListView ? "border-background border-2 bg-primary" : ""
-                    }`}></div>
-                </div>
-                <div>캘린더뷰</div>
-              </div>
+            />
+            <div className="px-10">
+                <Link href={'/admin/block/calendar/form'}>
+                    <button className="button color">
+                        + 캘린더에 일정을 추가하세요
+                    </button>
+                </Link>
             </div>
-          </div>
 
-          {isListView ? (
-            <div className="relative z-20 mx-auto flex w-full flex-col gap-10">
-              <PreblockCalendarOne flag={1} />
+            <div className="mb-10 h-3 w-full bg-gray-100" />
+
+            <div className="px-10">
+                <div>
+                    <p className="mb-4 text-2xl font-semibold text-gray-700">
+                        스타일설정
+                    </p>
+                    <div className="mb-4 flex flex-wrap">
+                        <div className="flex gap-4">
+                            <div
+                                className="flex cursor-pointer"
+                                onClick={() => setIsListView(true)}
+                            >
+                                <div
+                                    className={`circle mr-2 flex h-6 w-6 items-center justify-center rounded-full border-2 ${
+                                        isListView
+                                            ? 'bg-background border-4 border-orange-500'
+                                            : 'border-gray-200'
+                                    }`}
+                                >
+                                    <div
+                                        className={`button-color h-3 w-3 rounded-full ${
+                                            isListView
+                                                ? 'border-background border-2 bg-primary'
+                                                : ''
+                                        }`}
+                                    ></div>
+                                </div>
+                                <div>리스트뷰</div>
+                            </div>
+                            <div
+                                className="flex cursor-pointer"
+                                onClick={() => setIsListView(false)}
+                            >
+                                <div
+                                    className={`circle mr-2 flex h-6 w-6 items-center justify-center rounded-full border-2 ${
+                                        !isListView
+                                            ? 'bg-background border-4 border-primary'
+                                            : 'border-gray-200'
+                                    }`}
+                                >
+                                    <div
+                                        className={`button-color h-3 w-3 rounded-full ${
+                                            !isListView
+                                                ? 'border-background border-2 bg-primary'
+                                                : ''
+                                        }`}
+                                    ></div>
+                                </div>
+                                <div>캘린더뷰</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {isListView ? (
+                        <div className="relative z-20 mx-auto flex w-full flex-col gap-10">
+                            <PreblockCalendarOne flag={1} />
+                        </div>
+                    ) : (
+                        <PreblockCalendarTwo />
+                    )}
+                </div>
             </div>
-          ) : (
-            <PreblockCalendarTwo />
-          )}
-        </div>
-      </div>
 
-      <div className="my-10 w-full border" />
+            <div className="my-10 w-full border" />
 
-      <p className="mb-5 px-10 text-2xl font-semibold text-gray-700">
-        추가된 모든 일정
-      </p>
-      <div className="flex flex-col gap-4">
-        <div className="flex flex-wrap gap-4 border-b px-10 text-2xl font-semibold">
-          {/* 진행/예정된 일정 버튼 */}
-          <div
-            onClick={() => setViewTypeNow(true)}
-            className={`cursor-pointer pb-2 ${
-              viewTypeNow === true
-                ? "border-b border-primary text-primary"
-                : "text-gray-400"
-            }`}>
-            진행/예정된
-          </div>
-          <div
-            onClick={() => setViewTypeNow(false)}
-            className={`cursor-pointer pb-2 ${
-              viewTypeNow === false
-                ? "border-b border-primary text-primary"
-                : "text-gray-400"
-            }`}>
-            지난
-          </div>
-        </div>
-        <div className="mb-5 px-10 py-5 h-[20rem] overflow-y-scroll [&::-webkit-scrollbar]:hidden">
-          {viewTypeNow === true ? (
-            //  데이터세트 들어오면 로직 수정
-            dataSet === true ? (
-              <PreblockCalendarOne flag={2} />
-            ) : (
-              <div className="min-h-1/2 flex w-full flex-1 flex-col items-center justify-center rounded-md bg-gray-100 p-12">
-                <p className="text-center">
-                  표시할 블록이 없습니다.
-                  <br />+ 버튼을 눌러서 <strong>블록을 추가</strong>
-                  해보세요!
-                </p>
-                <Image
-                  src={"/assets/icons/icon_calendar.png"}
-                  alt="no schedule"
-                  width={50}
-                  height={50}
-                />
-              </div>
-            )
-          ) : (
-            <>
-              <div className="min-h-1/2 flex w-full flex-1 flex-col items-center justify-center rounded-md bg-gray-100 p-12">
-                <p className="text-center">
-                  표시할 블록이 없습니다.
-                  <br />+ 버튼을 눌러서 <strong>블록을 추가</strong>
-                  해보세요!
-                </p>
-                <Image
-                  src={"/assets/icons/icon_calendar.png"}
-                  alt="no schedule"
-                  width={50}
-                  height={50}
-                />
-              </div>
-            </>
-          )}
-        </div>
-      </div>
-    </>
-  );
+            <p className="mb-5 px-10 text-2xl font-semibold text-gray-700">
+                추가된 모든 일정
+            </p>
+            <div className="flex flex-col gap-4">
+                <div className="flex flex-wrap gap-4 border-b px-10 text-2xl font-semibold">
+                    {/* 진행/예정된 일정 버튼 */}
+                    <div
+                        onClick={() => setViewTypeNow(true)}
+                        className={`cursor-pointer pb-2 ${
+                            viewTypeNow === true
+                                ? 'border-b border-primary text-primary'
+                                : 'text-gray-400'
+                        }`}
+                    >
+                        진행/ 정된
+                    </div>
+                    <div
+                        onClick={() => setViewTypeNow(false)}
+                        className={`cursor-pointer pb-2 ${
+                            viewTypeNow === false
+                                ? 'border-b border-primary text-primary'
+                                : 'text-gray-400'
+                        }`}
+                    >
+                        지난
+                    </div>
+                </div>
+                <div className="mb-5 h-[20rem] overflow-y-scroll px-10 py-5 [&::-webkit-scrollbar]:hidden">
+                    {viewTypeNow === true ? (
+                        //  데이터세트 들어오면 로직 수정
+                        dataSet === true ? (
+                            <PreblockCalendarOne flag={2} />
+                        ) : (
+                            <div className="min-h-1/2 flex w-full flex-1 flex-col items-center justify-center rounded-md bg-gray-100 p-12">
+                                <p className="text-center">
+                                    표시할 블록이 없습니다.
+                                    <br />+ 버튼을 눌러서{' '}
+                                    <strong>블록을 추가</strong>
+                                    해보세요!
+                                </p>
+                                <Image
+                                    src={'/assets/icons/icon_calendar.png'}
+                                    alt="no schedule"
+                                    width={50}
+                                    height={50}
+                                />
+                            </div>
+                        )
+                    ) : (
+                        <>
+                            <div className="min-h-1/2 flex w-full flex-1 flex-col items-center justify-center rounded-md bg-gray-100 p-12">
+                                <p className="text-center">
+                                    표시할 블록이 없습니다.
+                                    <br />+ 버튼을 눌러서{' '}
+                                    <strong>블록을 추가</strong>
+                                    해보세요!
+                                </p>
+                                <Image
+                                    src={'/assets/icons/icon_calendar.png'}
+                                    alt="no schedule"
+                                    width={50}
+                                    height={50}
+                                />
+                            </div>
+                        </>
+                    )}
+                </div>
+            </div>
+        </>
+    );
 }
--- src/service/api/block-api.ts
+++ src/service/api/block-api.ts
@@ -37,3 +37,23 @@ export const updateBlock = async (params: UpdateBlockParams) => {
         throw new Error('Failed to add block');
     }
 };
+
+// 블록 업데이트 API 호출 함수
+export const updateCalBlock = async (params: AddBlockParams) => {
+    const { accessToken, blockData } = params;
+    const response = await fetch('/api/link/update', {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(blockData),
+    });
+
+    if (response.ok) {
+        const result = await response.json();
+        return result.data;
+    } else {
+        throw new Error('Failed to add block');
+    }
+};
